3de8a2160e2ed99e0316347186dfd1a04b45b262
ednakiruai <ednakiruai@gmail.com>
2024-03-21 23:37:56 +0300

    trial

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Write your solution here!
+let cats =["Milo","Otis","Garfield"]
+function destructivelyAppendCat(){
+    cats.push('Ralph')
+}
+function destructivelyPrependCat(){
+    cats.unshift('Bob')
+}
+function destructivelyRemoveLastCat(){
+    cats.pop()
+}
+function destructivelyRemoveFirstCat(){
+    cats.shift()
+}
+function appendCat(){
+    const cats=["Milo","Otis","Garfield"]
+    const newCats =[...cats,"Broom"];
+    return newCats
+}
+function prependCat(){
+    const cats =["Milo","Otis","Garfield"]
+    const allCats =["Arnold",...cats];
+    return allCats
+}
+function removeLastCat(){
+    const cats =["Milo","Otis","Garfield"]
+    const removeCat= cats.slice(0,cats.length-1);
+    return removeCat
+}
+function removeFirstCat(){
+    const cats =["Milo","Otis","Garfield"]
+    const sliceCat= cats.slice(1)
+    return sliceCat
+}
